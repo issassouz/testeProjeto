7ee1759082ccee9946faaa446d22b389fb66692d
Isabelle <isabellesouzalima80gmail.com>
2025-09-18 22:12:58 -0300

    Aula PAM- projeto teste

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Login from "./Components/Login/Login";
+import Teste from "./Components/Login/Teste";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Teste />
+    </div>
+  );
+}
+
+export default App;
